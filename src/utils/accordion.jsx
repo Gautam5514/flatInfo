98cc6330e0807fd7c131abd9451b342e04551736
Gautam pandit <gautampandit797@gmail.com>
2025-02-15 12:44:29 +0530

    This is the dummy project

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the market",
+    detail:
+      "Get the best interest rates available in the market with our comprehensive financial solutions tailored to your needs.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Our services help you avoid the pitfalls of unstable prices, ensuring a more predictable and secure financial future.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "We offer the most competitive prices in the market, backed by thorough analytics and market research.",
+  },
+];
+export default data;
